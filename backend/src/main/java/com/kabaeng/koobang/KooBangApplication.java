ad92f84b28621d105f2a229adea502eeceb975fe
이병희 <milk1543@naver.com>
2022-05-09 16:20:17 +0900

    Upload Spring project
    - Spring 프로젝트 생성 및 업로드

---
+++ backend/src/main/java/com/kabaeng/koobang/KooBangApplication.java
@@ -0,0 +1,13 @@
+package com.kabaeng.koobang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KooBangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KooBangApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/kabaeng/koobang/KooBangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kabaeng.koobang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KooBangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
