b2e36e8051a2e90a5ba85988e7dadecfe67d1869
이병희 <milk1543@naver.com>
2022-05-12 19:51:44 +0900

    Update sql file
    - sql 파일 sql 문 수정

--- documents/SQL/KooBang_sql.sql
+++ documents/SQL/KooBang_sql.sql
@@ -1,87 +1,97 @@
+-- drop table Consumer_Info;
+-- drop table Product_Info;
+-- drop table Vendor_Info;
+-- drop table Consumer_Address;
+-- drop table Basket;
+-- drop table Order_List;
+-- drop table Favorite_List;
+-- drop table Product_Review;
+-- drop table Advertising;
+
 ﻿CREATE TABLE `Consumer_Info` (
-	`consumer_id`	Integer	NOT NULL,
-	`consumer_email`	varchar(50)	NOT NULL,
-	`consumer_pwd`	varchar(100)	NOT NULL,
-	`consumer_name`	varchar(10)	NOT NULL,
-	`consumer_phone`	varchar(10)	NOT NULL
+	`id` INT NOT NULL,
+	`consumer_email` VARCHAR(50)	NOT NULL,
+	`consumer_pwd`	VARCHAR(100)	NOT NULL,
+	`consumer_name`	VARCHAR(10)	NOT NULL,
+	`consumer_phone`	VARCHAR(10)	NOT NULL
 );
 
 CREATE TABLE `Product_Info` (
-	`product_id`	Integer	NOT NULL,
-	`vendor_id`	Integer	NOT NULL,
-	`product_name`	varchar(50)	NOT NULL,
-	`product_image`	varchar(100)	NOT NULL,
-	`product_main_category`	varchar(20)	NOT NULL,
-	`product_middle_category`	varchar(20)	NOT NULL,
-	`product_subcategory`	varchar(20)	NOT NULL,
-	`product_brand`	varchar(20)	NULL,
-	`product_price`	Integer	NOT NULL,
-	`product_discount`	Integer	NOT NULL,
-	`product_quantity`	Integerr	NOT NULL,
-	`shipping_fee`	Integer	NOT NULL,
-	`shipping_free_price`	Integer	NOT NULL,
-	`shipping_time`	varchar(20)	NOT NULL,
-	`shipping_way`	varchar(20)	NOT NULL
+	`product_id`	INT	NOT NULL,
+	`vendor_id`	INT	NOT NULL,
+	`product_name`	VARCHAR(50)	NOT NULL,
+	`product_image`	VARCHAR(100)	NOT NULL,
+	`product_main_category`	VARCHAR(20)	NOT NULL,
+	`product_middle_category`	VARCHAR(20)	NOT NULL,
+	`product_subcategory`	VARCHAR(20)	NOT NULL,
+	`product_brand`	VARCHAR(20)	NULL,
+	`product_price`	INT	NOT NULL,
+	`product_discount`	INT	NOT NULL,
+	`product_quantity`	INT	NOT NULL,
+	`shipping_fee`	INT	NOT NULL,
+	`shipping_free_price`	INT	NOT NULL,
+	`shipping_time`	VARCHAR(20)	NOT NULL,
+	`shipping_way`	VARCHAR(20)	NOT NULL
 );
 
 CREATE TABLE `Consumer_Address` (
-	`address_id`	Integer	NOT NULL,
-	`consumer_id`	Integer	NOT NULL,
-	`receiver_name`	varchar(10)	NOT NULL,
-	`receiver_address`	varchar(50)	NOT NULL,
-	`receiver_phone`	varchar(10)	NOT NULL,
-	`request`	varchar(30)	NULL,
-	`access_number`	varchar(10)	NULL
+	`address_id`	INT	NOT NULL,
+	`consumer_id`	INT	NOT NULL,
+	`receiver_name`	VARCHAR(10)	NOT NULL,
+	`receiver_address`	VARCHAR(50)	NOT NULL,
+	`receiver_phone`	VARCHAR(10)	NOT NULL,
+	`request`	VARCHAR(30)	NULL,
+	`access_number`	VARCHAR(10)	NULL
 );
 
 CREATE TABLE `Product_Review` (
-	`review_id`	Integer	NOT NULL,
-	`consumer_id`	Integer	NOT NULL,
-	`product_id`	Integer	NOT NULL,
-	`review_summary`	varchar(10)	NULL,
-	`review_contents`	blob	NULL,
-	`review_image`	varchar(100)	NULL,
-	`review_rating`	Integer	NOT NULL,
-	`review_date`	datetime	NOT NULL,
-	`vendor_id`	Integer	NOT NULL
+	`review_id`	INT	NOT NULL,
+	`consumer_id`	INT	NOT NULL,
+	`product_id`	INT	NOT NULL,
+	`review_summary`	VARCHAR(10)	NULL,
+	`review_contents`	BLOB	NULL,
+	`review_image`	VARCHAR(100)	NULL,
+	`review_rating`	INT	NOT NULL,
+	`review_date`	DATE	NOT NULL,
+	`vendor_id`	INT	NOT NULL
 );
 
 CREATE TABLE `Order_List` (
-	`order_id`	Integer	NOT NULL,
-	`consumer_id`	Integer	NOT NULL,
-	`product_id`	Integer	NOT NULL,
-	`order_quantity`	Integer	NOT NULL,
-	`vendor_id`	Integer	NOT NULL
+	`order_id`	INT	NOT NULL,
+	`consumer_id`	INT	NOT NULL,
+	`product_id`	INT	NOT NULL,
+	`order_quantity`	INT	NOT NULL,
+	`vendor_id`	INT	NOT NULL
 );
 
 CREATE TABLE `Basket` (
-	`consumer_id`	Integer	NOT NULL,
-	`product_id`	Integer	NOT NULL,
-	`order_quantity`	Integer	NOT NULL,
-	`vendor_id`	Integer	NOT NULL
+	`consumer_id`	INT	NOT NULL,
+	`product_id`	INT	NOT NULL,
+	`order_quantity`	INT	NOT NULL,
+	`vendor_id`	INT	NOT NULL
 );
 
 CREATE TABLE `Advertising` (
-	`ad_id`	Integer	NOT NULL,
-	`product_id`	Integer	NOT NULL,
-	`vendor_id`	Integer	NOT NULL
+	`ad_id`	INT	NOT NULL,
+	`product_id`	INT	NOT NULL,
+	`vendor_id`	INT	NOT NULL
 );
 
-CREATE TABLE `Favorites_List` (
-	`consumer_id`	Integer	NOT NULL,
-	`product_id`	Integer	NOT NULL,
-	`vendor_id`	Integer	NOT NULL
+CREATE TABLE `Favorite_List` (
+	`consumer_id`	INT	NOT NULL,
+	`product_id`	INT	NOT NULL,
+	`vendor_id`	INT	NOT NULL
 );
 
 CREATE TABLE `Vendor_Info` (
-	`vendor_id`	Integer	NOT NULL,
-	`vendor_name`	varchar(50)	NOT NULL,
-	`vendor_registration_number`	varchar(10)	NOT NULL,
-	`vendor_phone`	varchar(10)	NOT NULL
+	`id`	INT	NOT NULL,
+	`vendor_name`	VARCHAR(50)	NOT NULL,
+	`vendor_registration_number`	VARCHAR(10)	NOT NULL,
+	`vendor_phone`	VARCHAR(10)	NOT NULL
 );
 
 ALTER TABLE `Consumer_Info` ADD CONSTRAINT `PK_CONSUMER_INFO` PRIMARY KEY (
-	`consumer_id`
+	`id`
 );
 
 ALTER TABLE `Product_Info` ADD CONSTRAINT `PK_PRODUCT_INFO` PRIMARY KEY (
@@ -114,47 +124,47 @@ ALTER TABLE `Advertising` ADD CONSTRAINT `PK_ADVERTISING` PRIMARY KEY (
 	`vendor_id`
 );
 
-ALTER TABLE `Favorites_List` ADD CONSTRAINT `PK_FAVORITES_LIST` PRIMARY KEY (
+ALTER TABLE `Favorite_List` ADD CONSTRAINT `PK_Favorite_LIST` PRIMARY KEY (
 	`consumer_id`
 );
 
 ALTER TABLE `Vendor_Info` ADD CONSTRAINT `PK_VENDOR_INFO` PRIMARY KEY (
-	`vendor_id`
+	`id`
 );
 
 ALTER TABLE `Product_Info` ADD CONSTRAINT `FK_Vendor_Info_TO_Product_Info_1` FOREIGN KEY (
 	`vendor_id`
 )
 REFERENCES `Vendor_Info` (
-	`vendor_id`
+	`id`
 );
 
 ALTER TABLE `Consumer_Address` ADD CONSTRAINT `FK_Consumer_Info_TO_Consumer_Address_1` FOREIGN KEY (
 	`consumer_id`
 )
 REFERENCES `Consumer_Info` (
-	`consumer_id`
+	`id`
 );
 
 ALTER TABLE `Product_Review` ADD CONSTRAINT `FK_Consumer_Info_TO_Product_Review_1` FOREIGN KEY (
 	`consumer_id`
 )
 REFERENCES `Consumer_Info` (
-	`consumer_id`
+	`id`
 );
 
 ALTER TABLE `Order_List` ADD CONSTRAINT `FK_Consumer_Info_TO_Order_List_1` FOREIGN KEY (
 	`consumer_id`
 )
 REFERENCES `Consumer_Info` (
-	`consumer_id`
+	`id`
 );
 
 ALTER TABLE `Basket` ADD CONSTRAINT `FK_Consumer_Info_TO_Basket_1` FOREIGN KEY (
 	`consumer_id`
 )
 REFERENCES `Consumer_Info` (
-	`consumer_id`
+	`id`
 );
 
 ALTER TABLE `Advertising` ADD CONSTRAINT `FK_Product_Info_TO_Advertising_1` FOREIGN KEY (
@@ -171,10 +181,10 @@ REFERENCES `Product_Info` (
 	`vendor_id`
 );
 
-ALTER TABLE `Favorites_List` ADD CONSTRAINT `FK_Consumer_Info_TO_Favorites_List_1` FOREIGN KEY (
+ALTER TABLE `Favorite_List` ADD CONSTRAINT `FK_Consumer_Info_TO_Favorite_List_1` FOREIGN KEY (
 	`consumer_id`
 )
 REFERENCES `Consumer_Info` (
-	`consumer_id`
+	`id`
 );
 
